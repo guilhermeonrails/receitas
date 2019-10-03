a7521876cc31074826ed0fbbb7ed271d1d1fef06
Guilherme Lima <guilhermelimaruby@gmail.com>
2019-10-03 13:32:50 -0300

    dicionario categorias

---
+++ receitas/categorias.py
@@ -0,0 +1,14 @@
+tipos_de_receitas = {
+    'doces':' Doces',
+    'arroz':'Arroz',
+    'bolos':'Bolos',
+    'caldos':'Caldos',
+    'natal':'Natal',
+    'lasanhas':'Lasanhas',
+    'legumes':'Legumes',
+    'massas':'Massas',
+    'molhos':'Molhos',
+    'salada':'Salada',
+    'sanduiches':'Sanduiches',
+    'sopas':'Sopas'
+} 
--- receitas/views.py
+++ receitas/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import Receita
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from receitas.categorias import tipos_de_receitas
 
 def index(request):
     receitas = Receita.objects.order_by('-data_receita').filter(publicada=True)
@@ -17,4 +18,11 @@ def contato(request):
     return render(request, 'contato.html')
 
 def receita(request, receita_id):
-    return render(request, 'receita.html')
+    receita = get_object_or_404(Receita,pk=receita_id)
+
+    receita_a_exibir = {
+        'receita' : receita,
+        'tipos_de_receitas' : tipos_de_receitas
+    }
+
+    return render(request, 'receita.html', receita_a_exibir)
